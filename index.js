3d78c594341de0234715d2b6f073f8e18ed05f5b
Hugo Giraudel <hugo.giraudel@gmail.com>
2016-04-18 16:07:23 +0200

    Added index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+(function (global) {
+  'use strict'
+
+  const InlineStyler = function (styleAttribute) {
+    this.styles = this.parseStyles(styleAttribute || '')
+  }
+
+  InlineStyler.prototype.getStyles = function getStyles () {
+    return this.styles
+  }
+
+  InlineStyler.prototype.hasStyles = function hasStyles () {
+    return Object.keys(this.styles).length > 0
+  }
+
+  InlineStyler.prototype.parseStyles = function parseStyles (styleAttribute) {
+    return styleAttribute
+      .split(';')
+      .reduce((accumulator, declaration) => {
+        const chunks = declaration.split(/\s*:\s*/)
+        if (chunks.length > 1) {
+          const property = chunks[0].trim()
+          const value = chunks[1].trim()
+          accumulator[property] = value
+        }
+        return accumulator
+      }, {})
+  }
+
+  InlineStyler.prototype.hasStyle = function hasStyle (property) {
+    return typeof this.styles[property] !== 'undefined'
+  }
+
+  InlineStyler.prototype.setStyle = function setStyle (property, value) {
+    if (typeof property === 'object' && !value) {
+      return this.setStyles(property)
+    }
+
+    this.styles[property] = value
+    return this
+  }
+
+  InlineStyler.prototype.setStyles = function setStyles (object) {
+    for (var property in object) {
+      this.setStyle(property, object[property])
+    }
+
+    return this
+  }
+
+  InlineStyler.prototype.unsetStyle = function unsetStyle (property) {
+    delete this.styles[property]
+    return this
+  }
+
+  InlineStyler.prototype.getStyle = function getStyle (property) {
+    return this.styles[property]
+  }
+
+  InlineStyler.prototype.toString = function () {
+    return Object.keys(this.styles)
+      .reduce((accumulator, property) => {
+        accumulator.push(property + ': ' + this.styles[property] + ';')
+        return accumulator
+      }, [])
+      .join(' ')
+  }
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = InlineStyler;
+  } else {
+    window.InlineStyler = InlineStyler;
+  }
+
+}(this))
